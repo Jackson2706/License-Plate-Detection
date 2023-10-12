17677a3d89be8c185c7c366a01e618a56fdf0cd1
Jackson2706 <dung27062002@gmail.com>
2023-10-12 22:35:17 +0700

    Combine clf and localization function

--- main.py
+++ main.py
@@ -1,4 +1,40 @@
+from cv2 import imread
 from joblib import load
+from numpy import argmax
+from utils.localization_process import pyramid, sliding_window
+from utils.classification_preprocess import preprocess_img
 
-loaded_model, loaded_scaler = load("Weights/clf_model_and_scaler_feature.pkl")
-print(loaded_model)
+image_path = "5587_jpg.rf.026eb4698e8035d73abea983bb0ef785.jpg"
+window_size = [
+    (32,32),
+    (64,64),
+    (128,128)
+]
+stride = 12
+conf_threshold = 0.8
+image = imread(image_path)
+
+clf, scaler, label_encoder = load("weights/clf_model_and_scaler_feature.pkl")
+
+
+pyramid_imgs = pyramid(image)
+bboxes = []
+for pyramid_img_info in pyramid_imgs:
+    pyramid_img, scale_factor = pyramid_img_info
+    window_lst = sliding_window(pyramid_img,
+                                window_sizes=window_size,
+                                stride=stride
+                                )
+    for window in window_lst:
+        xmin, ymin, xmax, ymax = window
+        object_img = pyramid_img[ymin:ymax, xmin:xmax]
+        preprocess_img = preprocess_img(object_img)
+        normalize_img = scaler.transform([preprocess_img])[0]
+        decision = clf.predict_proba([normalize_img])[0]
+        if np.all(decision < conf_threshold):
+            continue
+        predict_id = argmax(decision)
+        conf_score = decision[predict_id]
+        bboxes.append(
+            [xmin, ymin, xmax, ymax, predict_id, conf_score])
+        )

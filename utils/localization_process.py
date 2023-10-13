e5d5b1a808ef3189147000ded3fa46ee1c0e36c8
Jackson2706 <dung27062002@gmail.com>
2023-10-13 19:19:43 +0700

    remove plot function to metrics.py

--- utils/localization_process.py
+++ utils/localization_process.py
@@ -1,6 +1,5 @@
-from cv2 import resize, cvtColor, COLOR_BGR2RGB, rectangle, getTextSize, FONT_HERSHEY_SIMPLEX, putText
-from matplotlib.pyplot import imshow, axis, show
-
+from cv2 import resize
+from numpy import array, argsort, maximum, minimum, where
 
 
 def sliding_window(img, window_sizes, stride):
@@ -14,6 +13,7 @@ def sliding_window(img, window_sizes, stride):
                 y_max = y_min + window_height
                 windows.append([x_min, y_min, x_max, y_max])
     return windows
+
 
 def pyramid(img, scale=0.8, min_size=(30,30)):
     acc_scale = 1.0
@@ -30,18 +30,60 @@ def pyramid(img, scale=0.8, min_size=(30,30)):
         i =  i + 1
     return pyramid_imgs
 
-def visualize_bbox(img, bboxes, label_encoder):
-    img = cvtColor(img, COLOR_BGR2RGB)
-    for box in bboxes:
-        x_min, y_min, x_max, y_max, predict_id, conf_score = box
-        rectangle(img, (x_min, y_min), (x_max, y_max), (0,255,0), 2)
-        class_name = label_encoder.inverse_transform([predict_id])[0]
-        label = f"{class_name} {conf_score}"
-        (w,h), _ = getTextSize(label, FONT_HERSHEY_SIMPLEX, 0,.6, 1)
-        rectangle(img, (x_min, y_min-20),(x_min+w, y_min), (0,255,0), -1)
-        putText(img, label, (x_min, y_min-5), FONT_HERSHEY_SIMPLEX, 0.6, (0,0,0),1) 
 
-    imshow(img)
-    axis("off")
-    show()
+def compute_iou(box, boxes, box_area, boxes_area):
+    """Calculates IoU of the given box with the array of the given boxes.
+    box: 1D vector [y1, x1, y2, x2]
+    boxes: [boxes_count, (y1, x1, y2, x2)]
+    box_area: float. the area of 'box'
+    boxes_area: array of length boxes_count.
+    Note: the areas are passed in rather than calculated here for
+    efficiency. Calculate once in the caller to avoid duplicate work.
+    """
+    x1 = maximum(box[0], boxes[:, 0])
+    y1 = maximum(box[1], boxes[:, 1])
+    x2 = minimum(box[2], boxes[:, 2])
+    y2 = minimum(box[3], boxes[:, 3])
+    
+    intersection =  maximum((x2-x1), 0) * maximum((y2-y1), 0)
 
+    union = box_area + boxes_area[:] - intersection
+
+    iou = intersection * 1.0 / union
+
+    return iou
+
+def nms(bboxes, iou_threshold):
+    if not bboxes:
+        return []
+    scores = array([bbox[5] for bbox in bboxes])
+    sorted_indices = argsort(scores)[::-1]
+
+    xmin = array([bbox[0] for bbox in bboxes])
+    ymin = array([bbox[1] for bbox in bboxes])
+    xmax = array([bbox[2] for bbox in bboxes])
+    ymax = array([bbox[3] for bbox in bboxes])
+
+    areas = (xmax - xmin + 1) * (ymax - ymin + 1)
+
+    keep = []
+
+    while sorted_indices.size > 0:
+        i = sorted_indices[0]
+        keep.append(i)
+
+        iou = compute_iou(
+            [xmin[i], ymin[i],xmax[i],ymax[i]],
+            array(
+                [xmin[sorted_indices[1:]],
+                 ymin[sorted_indices[1:]],
+                 xmax[sorted_indices[1:]],
+                 ymax[sorted_indices[1:]]]
+            ).T,
+            areas[i],
+            areas[sorted_indices[1:]]
+        )
+
+        idx_to_keep = where(iou <= iou_threshold)[0]
+        sorted_indices = sorted_indices[idx_to_keep + 1]
+    return [bboxes[i] for i in keep]

b6f1cfd99b895254553d894e2a2954eb78955f50
Jackson2706 <dung27062002@gmail.com>
2023-10-13 11:24:13 +0700

    optimize import lib

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from cv2 import imread
 from joblib import load
-from numpy import argmax
+from numpy import argmax, all
 from utils.localization_process import pyramid, sliding_window
 from utils.classification_preprocess import preprocess_img
 
@@ -16,7 +16,6 @@ image = imread(image_path)
 
 clf, scaler, label_encoder = load("weights/clf_model_and_scaler_feature.pkl")
 
-
 pyramid_imgs = pyramid(image)
 bboxes = []
 for pyramid_img_info in pyramid_imgs:
@@ -28,13 +27,12 @@ for pyramid_img_info in pyramid_imgs:
     for window in window_lst:
         xmin, ymin, xmax, ymax = window
         object_img = pyramid_img[ymin:ymax, xmin:xmax]
-        preprocess_img = preprocess_img(object_img)
-        normalize_img = scaler.transform([preprocess_img])[0]
-        decision = clf.predict_proba([normalize_img])[0]
-        if np.all(decision < conf_threshold):
+        preprocessed_img = preprocess_img(object_img)
+        normalized_img = scaler.transform([preprocessed_img])[0]
+        decision = clf.predict_proba([normalized_img])[0]
+        if all(decision < conf_threshold):
             continue
         predict_id = argmax(decision)
         conf_score = decision[predict_id]
-        bboxes.append(
-            [xmin, ymin, xmax, ymax, predict_id, conf_score])
-        )
+        bboxes.append([xmin, ymin, xmax, ymax, predict_id, conf_score])
+        
